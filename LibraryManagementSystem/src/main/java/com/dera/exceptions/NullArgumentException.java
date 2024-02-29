0481658b1d0492ecfd4705b9b438b37dcef9c064
Deraclassic <chiderauzoigwe2@gmail.com>
2024-02-29 16:44:45 +0100

    commit files

--- LibraryManagementSystem/src/main/java/com/dera/exceptions/NullArgumentException.java
+++
@@ -1,7 +0,0 @@
-package com.dera.exceptions;
-
-public class NullArgumentException extends IllegalArgumentException {
-    public NullArgumentException(String message) {
-        super(message);
-    }
-}
---
+++ LibraryManagementSystem/src/main/java/com/dera/exceptions/UserNotFoundException.java
@@ -0,0 +1,2 @@
+package com.dera.exceptions;public class UserNotFoundException {
+}
